c0372a0bde23fc1298469b3c96c2d04714bd6782
Rakhmatullo <qosimovrahmatullo006@gmail.com>
2024-11-14 10:46:32 +0500

    feat: vercel web analytics

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
+import { Analytics } from '@vercel/analytics/vue';
+
 import I18nProvider from './components/I18nProvider.vue';
 </script>
 <template>
+  <Analytics />
   <I18nProvider>
     <router-view />
   </I18nProvider>
