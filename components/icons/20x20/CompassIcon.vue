a0577892bb0b30e2d3142bb93fc46fcd3078f26c
Rakhmatullo <qosimovrahmatullo006@gmail.com>
2024-11-25 03:03:27 +0500

    feat: new CompassIcon

---
+++ components/icons/20x20/CompassIcon.vue
@@ -0,0 +1,14 @@
+<template>
+  <svg
+    width="20"
+    height="20"
+    viewBox="0 0 20 20"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M8.41435 12.3018L1 11.028L17 3.5L9.26352 19.5L8.41435 12.3018Z"
+      fill="currentColor"
+    />
+  </svg>
+</template>
