51318a306d32b8d34df6284a34079673393d9bfb
Rakhmatullo <qosimovrahmatullo006@gmail.com>
2024-11-27 18:28:21 +0500

    feat: add default semantic color for better default color

--- presets/Noir.ts
+++ presets/Noir.ts
@@ -3,6 +3,19 @@ import Aura from '@primevue/themes/aura'
 
 const Noir = definePreset(Aura, {
   semantic: {
+    primary: {
+      50: '{surface.50}',
+      100: '{surface.100}',
+      200: '{surface.200}',
+      300: '{surface.300}',
+      400: '{surface.400}',
+      500: '{surface.500}',
+      600: '{surface.600}',
+      700: '{surface.700}',
+      800: '{surface.800}',
+      900: '{surface.900}',
+      950: '{surface.950}',
+    },
     // suppose we have noir preset
   },
 })
