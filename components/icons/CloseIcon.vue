fd08a2fa03baa4887adbdeb8f53fbac534241d24
Rakhmatullo <qosimovrahmatullo006@gmail.com>
2024-11-27 22:27:00 +0500

    feat(components): new close icon component

---
+++ components/icons/CloseIcon.vue
@@ -0,0 +1,15 @@
+<template>
+  <svg
+    width="20"
+    height="20"
+    viewBox="0 0 20 20"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M3.33333 3.33331L9.99999 9.99998M16.6667 16.6666L9.99999 9.99998M16.6667 3.33331L9.99999 9.99998M3.33333 16.6666L9.99999 9.99998"
+      stroke="currentColor"
+      stroke-width="1.5"
+    />
+  </svg>
+</template>
