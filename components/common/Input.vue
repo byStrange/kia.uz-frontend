4ad7282a3752e869fc634e23b2f62e1560b365f7
Rakhmatullo <qosimovrahmatullo006@gmail.com>
2024-11-27 22:26:48 +0500

    feat(components): new input component

---
+++ components/common/Input.vue
@@ -0,0 +1,65 @@
+<script setup lang="ts">
+import { InputText, FloatLabel, type InputTextProps } from 'primevue'
+import { tv, type VariantProps } from 'tailwind-variants'
+
+const value = defineModel({ default: '' })
+
+const input = tv(
+  {
+    base: '!rounded-none hover:!border-caption focus:!border-primary !text-base+ !text-primary',
+
+    variants: {
+      theme: {
+        light: '!border-t-0 !border-x-0 !px-0 !border-b-protection',
+        default: '!border-protection !px-4 bg-white',
+      },
+      size: {
+        large: '!pt-6 !pb-3', // overally py-4.5
+        default: '!pt-5 !pb-1', // overall py-3
+      },
+    },
+  },
+  {
+    responsiveVariants: ['md', '2xl'],
+  },
+)
+
+type InputProps = VariantProps<typeof input>
+
+withDefaults(
+  defineProps<{
+    inputId: string
+    label: string
+    inputProps?: InputTextProps
+    theme?: InputProps['theme']
+    size?: InputProps['size']
+  }>(),
+  {
+    theme: 'default',
+    size: 'default',
+  },
+)
+</script>
+<template>
+  <FloatLabel
+    variant="in"
+    :class="'theme-' + theme"
+    :pt="{ root: { style: '--p-floatlabel-position-x: 16px' } }"
+  >
+    <InputText
+      :inputId="inputId"
+      v-bind="inputProps"
+      :pt="{
+        root: input({ theme, size: { initial: 'default', '2xl': 'large' } }),
+      }"
+      v-model="value"
+    />
+    <label :for="inputId" class="!text-caption">{{ label }}</label>
+  </FloatLabel>
+</template>
+
+<style>
+.theme-light.p-floatlabel label {
+  @apply left-0;
+}
+</style>
