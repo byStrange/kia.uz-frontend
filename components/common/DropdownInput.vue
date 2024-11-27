6db9114b2bfbaa4f9a6995ab6270e2a4db0b9bc3
Rakhmatullo <qosimovrahmatullo006@gmail.com>
2024-11-27 22:26:24 +0500

    feat(components): enhanced dropdown input

--- components/common/DropdownInput.vue
+++ components/common/DropdownInput.vue
@@ -44,7 +44,7 @@ const dropdown = tv({
     size: {
       default: {
         label: '!p-0',
-        root: 'py-3',
+        root: 'py-3 2xl:py-4.5',
         overlay: '!py-4 !px-3',
         option: '!py-2.5 !pl-7',
       },
@@ -68,13 +68,11 @@ withDefaults(
     inputId?: string
     size?: DropdownProps['size']
     theme?: DropdownProps['theme']
-    floatLabel?: boolean
   }>(),
   {
     inputId: '',
     theme: 'default',
     size: 'default',
-    floatLabel: false,
     optionLabel: 'label',
     optionValue: 'value',
     placeholder: '',
@@ -83,81 +81,21 @@ withDefaults(
 </script>
 
 <template>
-  <template v-if="floatLabel">
-    <FloatLabel variant="in" :class="'theme-' + theme">
-      <Select
-        v-model="selectedOption"
-        :options="availableOptions"
-        :option-label="optionLabel"
-        :option-value="optionValue"
-        :input-id="inputId"
-        class="w-full"
-        :pt="{
-          dropdown: (props) => {
-            return {
-              class: [
-                'transition-all !w-4 !h-4',
-                {
-                  // @ts-ignore
-                  'rotate-180': props.state.clicked,
-                },
-              ],
-            }
-          },
-          label: ({ props }) => {
-            return {
-              class: [
-                dropdown.variants.theme[theme].label.base,
-                dropdown.variants.size[size].label,
-                {
-                  [dropdown.variants.theme[theme].label.empty]:
-                    !props.modelValue,
-                  [dropdown.variants.theme[theme].label.filled]:
-                    props.modelValue,
-                },
-              ],
-            }
-          },
-          option: {
-            class: [
-              dropdown.variants.theme[theme].option,
-              dropdown.variants.size[size].option,
-            ],
-          },
-          root: {
-            class: [
-              dropdown.base,
-              dropdown.variants.theme[theme].root,
-              dropdown.variants.size[size].root,
-            ],
-          },
-
-          overlay: {
-            class: [
-              dropdown.variants.theme[theme].overlay,
-              dropdown.variants.size[size].overlay,
-            ],
-          },
-        }"
-      />
-      <label :for="inputId">{{ placeholder }}</label>
-    </FloatLabel>
-  </template>
-
-  <template v-else>
+  <FloatLabel variant="in" :class="'theme-' + theme">
     <Select
       v-model="selectedOption"
       :options="availableOptions"
       :option-label="optionLabel"
       :option-value="optionValue"
       :input-id="inputId"
-      :placeholder
       class="w-full"
       :pt="{
+        list: '!p-0',
+        optionLabel: 'w-[240px] text-balance md:w-auto',
         dropdown: (props) => {
           return {
             class: [
-              'transition-all w-4 h-4',
+              'transition-all !w-4 !h-4',
               {
                 // @ts-ignore
                 'rotate-180': props.state.clicked,
@@ -199,7 +137,8 @@ withDefaults(
         },
       }"
     />
-  </template>
+    <label :for="inputId">{{ placeholder }}</label>
+  </FloatLabel>
 </template>
 <style>
 .theme-light.p-floatlabel.p-floatlabel-in label {
