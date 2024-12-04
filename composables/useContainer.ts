480d45ec7542a68b69e7e52a7f2aa77d427779a3
Rakhmatullo <qosimovrahmatullo006@gmail.com>
2024-12-04 14:07:20 +0500

    fix(composable): useContainer test variable changed to containerInner

--- composables/useContainer.ts
+++ composables/useContainer.ts
@@ -1,21 +1,21 @@
 const container = ref<HTMLElement | null>(null)
-const test = ref<HTMLElement | null>(null)
+const containerInner = ref<HTMLElement | null>(null)
 
 if (import.meta.client) {
   container.value = document.createElement('div')
-  test.value = document.createElement('div')
+  containerInner.value = document.createElement('div')
 
-  test.value.style.width = '100%'
+  containerInner.value.style.width = '100%'
 
-  container.value.appendChild(test.value)
+  container.value.appendChild(containerInner.value)
   container.value.classList.add('container')
   container.value.style.zIndex = '-99'
   document.body.appendChild(container.value)
 }
 
 export const useContainer = () => {
-  const bounding = useElementBounding(test)
-  const offset = useOffset(test.value)
+  const bounding = useElementBounding(containerInner)
+  const offset = useOffset(containerInner.value)
 
   return {
     container,
