f75f589f3228eb2051ba67fb1cf95fd37e00ec74
Rakhmatullo <qosimovrahmatullo006@gmail.com>
2024-12-04 14:06:41 +0500

    fix(middleware): close header menu when route changes

--- middleware/route.global.ts
+++ middleware/route.global.ts
@@ -1,5 +1,7 @@
 export default defineNuxtRouteMiddleware((to, from) => {
   if (to.path !== from.path && import.meta.client) {
+    useHeaderService().headerService.value.isMenuOpen = false
+    useHeaderService().closeExtendedMenu()
     window.scrollTo(0, 0)
   }
 })
