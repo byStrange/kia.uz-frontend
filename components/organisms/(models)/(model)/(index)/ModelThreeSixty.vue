26ce504b858ae6b34176bb36500075720a04ce9e
Rakhmatullo <qosimovrahmatullo006@gmail.com>
2025-01-13 12:49:00 +0500

    feat(composable): new use360View composable, and test it in few pages

--- components/organisms/(models)/(model)/(index)/ModelThreeSixty.vue
+++ components/organisms/(models)/(model)/(index)/ModelThreeSixty.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 const modelType = ref('exterior')
 const selectedColor = ref()
+
 const availableColors = ref([
   '#F3F3F3',
   '#B6B7B2',
@@ -9,6 +10,20 @@ const availableColors = ref([
   '#7E2E35',
   '#8F9998',
 ])
+
+const threeSixtyViewRef = ref(null)
+
+const data = use360View(threeSixtyViewRef, {
+
+  images: 'https://cdn.kia.uz/exterior/Cerato/B4U/',
+  extension: 'png',
+  prefix: '',
+  totalImages: 72,
+  preloadBeforeMount: true,
+  sensitivity: 10,
+  inertia: 0.2,
+
+})
 
 const { src } = useUploadcareSource()
 </script>
@@ -20,25 +35,13 @@ const { src } = useUploadcareSource()
         Carnival
       </h2>
     </div>
-    <picture>
-      <source
-        :srcset="
-          src('9a342c87-7ca1-43d4-abb0-1b505840f0c6', { preview: '768x432' })
-        "
-        media="(min-width: 1440px)"
-      />
-      <img
-        loading="lazy"
-        :src="
-          src('40aceb5c-0188-4362-bacd-86dd776ce86b', { preview: '1000x501' })
-        "
-        class="w-full my-4 2xl:mt-8 md:mt-6 2xl:mb-0 2xl:w-auto 2xl:mx-auto"
-      />
-    </picture>
+    <div ref="threeSixtyViewRef">
+      <img :draggable="false" loading="lazy" :src="data.currentSrc.value"
+        class="w-full my-4 2xl:mt-8 md:mt-6 2xl:mb-0 2xl:w-auto 2xl:mx-auto" />
+    </div>
     <div class="md:mt-6 2xl:mt-12 relative">
       <div
-        class="flex flex-col items-center gap-2.5 2xl:absolute 2xl:-top-10 2xl:left-1/2 2xl:-translate-x-1/2 2xl:max-w-6h mx-auto 2xl:px-20"
-      >
+        class="flex flex-col items-center gap-2.5 2xl:absolute 2xl:-top-10 2xl:left-1/2 2xl:-translate-x-1/2 2xl:max-w-6h mx-auto 2xl:px-20">
         <UIIcon360 />
         <p class="text-xs+ text-disabled md:text-center">
           Изображение может не соответствовать выбранной комплектации. <br />
@@ -49,33 +52,21 @@ const { src } = useUploadcareSource()
         <div>
           <div class="flex gap-5 text-base+">
             <div class="flex items-center gap-2.5">
-              <PrimeRadioButton
-                v-model="modelType"
-                value="exterior"
-                input-id="exterior"
-                name="type"
-                :pt="{
-                  icon: 'hidden',
-                  box: (state) => {
-                    return state.context.checked ? '!border-[6px] ' : '0'
-                  },
-                }"
-              />
+              <PrimeRadioButton v-model="modelType" value="exterior" input-id="exterior" name="type" :pt="{
+                icon: 'hidden',
+                box: (state) => {
+                  return state.context.checked ? '!border-[6px] ' : '0'
+                },
+              }" />
               <label for="exterior">Экстерьер</label>
             </div>
             <div class="flex items-center gap-2.5">
-              <PrimeRadioButton
-                v-model="modelType"
-                value="interior"
-                input-id="interior"
-                name="type"
-                :pt="{
-                  icon: 'hidden',
-                  box: (state) => {
-                    return state.context.checked ? '!border-[6px] ' : '0'
-                  },
-                }"
-              />
+              <PrimeRadioButton v-model="modelType" value="interior" input-id="interior" name="type" :pt="{
+                icon: 'hidden',
+                box: (state) => {
+                  return state.context.checked ? '!border-[6px] ' : '0'
+                },
+              }" />
               <label for="interior">Интерьер</label>
             </div>
           </div>
@@ -85,17 +76,10 @@ const { src } = useUploadcareSource()
               <b class="text-primary">Snow White Pearl (SWP)</b>
             </div>
             <div class="mt-4 flex gap-2.5">
-              <div
-                v-for="color in availableColors"
-                :key="color"
+              <div v-for="color in availableColors" :key="color"
                 class="color flex size-[35px] items-center justify-center rounded-full border border-disabled"
-                :style="{ backgroundColor: color }"
-                @click="selectedColor = color"
-              >
-                <UICheckIcon
-                  v-if="color === selectedColor"
-                  class="text-white"
-                />
+                :style="{ backgroundColor: color }" @click="selectedColor = color">
+                <UICheckIcon v-if="color === selectedColor" class="text-white" />
               </div>
             </div>
           </div>
---
+++ pages/test.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+definePageMeta({
+  lockHover: true
+})
+
+const imageContainer = ref()
+
+
+const all = use360View(imageContainer, {
+  images: 'https://cdn.kia.uz/exterior/Cerato/B4U/',
+  extension: 'png',
+  prefix: '',
+  totalImages: 72,
+  preloadBeforeMount: true,
+  sensitivity: 10,
+  inertia: 0.2,
+})
+</script>
+<template>
+  <UISafeAreaView>
+    <UIContainer>
+      <section class="min-h-screen" ref="imageContainer">
+        <img :src="all.currentSrc.value" :draggable="false" />
+      </section>
+    </UIContainer>
+  </UISafeAreaView>
+</template>
