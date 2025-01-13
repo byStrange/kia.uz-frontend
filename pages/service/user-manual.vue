a542ae2d06ee08c133c9ebd45edd65f135424099
Rakhmatullo <qosimovrahmatullo006@gmail.com>
2025-01-13 12:46:53 +0500

    fix: closed issue #137 wrong button references

--- pages/service/user-manual.vue
+++ pages/service/user-manual.vue
@@ -10,7 +10,7 @@ const { models } = useStore()
 <template>
   <UISafeAreaView>
     <UIContainer>
-      <div class="pt-10 pb-6 md:pb-5 pt-15 2xl:pt-6 2xl:pb-0">
+      <div class="pt-10 pb-6 md:pb-5 md:pt-15 2xl:pt-6 2xl:pb-0">
         <UIDesktopOnly>
           <MoleculeBreadcrumb theme="dark" />
         </UIDesktopOnly>
@@ -19,15 +19,12 @@ const { models } = useStore()
         </h1>
       </div>
       <div
-        class="pt-6 pb-15 space-y-12 md:grid md:grid-cols-2 md:gap-grid-12-gap md:space-y-0 2xl:grid-cols-4 2xl:pt-1h"
-      >
+        class="pt-6 pb-15 space-y-12 md:grid md:grid-cols-2 md:gap-grid-12-gap md:space-y-0 2xl:grid-cols-4 2xl:pt-1h">
         <div v-for="model in models.slice(0, 4)" :key="model.name">
           <img :src="src('0b0e228e-8e0e-4f06-b1b6-4974f45d1d0e')" alt="" />
           <div class="mt-5">
-            <h2
-              class="text-primary font-semibold text-base md:text-lg 2xl:text-2xl"
-            >
-              {{ model.title }}
+            <h2 class="text-primary font-semibold text-base md:text-lg 2xl:text-2xl">
+              {{ model.name }}
             </h2>
             <button class="flex gap-x-2 mt-2 cursor-pointer">
               <UIPDFIcon class="text-primary" />
@@ -41,19 +38,13 @@ const { models } = useStore()
 
       <div class="pb-12 md:grid md:grid-cols-2 rtl">
         <picture>
-          <source
-            :srcset="src('b233ed8a-dc9a-4e68-9fb2-24cbe43f99ba')"
-            media="(min-width: 768px) and (max-width: 1440px)"
-          />
-          <img
-            :src="src('062491d2-01fb-48b1-b730-43639a864d04')"
-            alt=""
-            class="object-cover md:w-full md:max-h-3h 2xl:max-h-none md:h-full 2xl:h-[388px]"
-          />
+          <source :srcset="src('b233ed8a-dc9a-4e68-9fb2-24cbe43f99ba')"
+            media="(min-width: 768px) and (max-width: 1440px)" />
+          <img :src="src('062491d2-01fb-48b1-b730-43639a864d04')" alt=""
+            class="object-cover md:w-full md:max-h-3h 2xl:max-h-none md:h-full 2xl:h-[388px]" />
         </picture>
         <div
-          class="py-8 px-4 bg-caption md:px-6 md:flex md:flex-col md:justify-between md:items-start 2xl:px-12 2xl:py-14 ltr"
-        >
+          class="py-8 px-4 bg-caption md:px-6 md:flex md:flex-col md:justify-between md:items-start 2xl:px-12 2xl:py-14 ltr">
           <div class="text-white">
             <h2 class="text-lg font-semibold md:text-2xl 2xl:text-5xl">
               Предупреждающие индикаторы
@@ -62,11 +53,7 @@ const { models } = useStore()
               Информируют о сервисных функциях автомобиля и их активности.
             </p>
           </div>
-          <AtomButton
-            mode="full"
-            label="Подробнее"
-            class="mt-6 !text-base+"
-          />
+          <AtomButton mode="full" color="secondary" label="Подробнее" class="mt-6 !text-base+" />
         </div>
       </div>
     </UIContainer>
--- pages/special-offers/offer:[slug].vue
+++ pages/special-offers/offer:[slug].vue
@@ -12,63 +12,37 @@ const { src } = useUploadcareSource()
       class="h-5h overflow-hidden grid place-items-end md:place-items-start md:pt-[--safe-area-padding-top] md:h-[420px] 2xl:h-6h 2xl:place-items-end relative"
       :style="{
         '--safe-area-padding-top': paddingTop,
-      }"
-    >
+      }">
       <div
         class="shadowman absolute w-full h-full top-0 left-0 z-10 bg-[image:--gradient] md:bg-[image:none] md:bg-primary md:bg-opacity-60"
         :style="{
           '--gradient':
             'linear-gradient(180deg, rgba(8, 20, 30, 0.4) 0%, rgba(8, 20, 30, 0.589696) 40.99%, #08141E 65.6%)',
-        }"
-      ></div>
+        }"></div>
       <picture>
-        <source
-          :srcset="
-            src('2ba0480c-303b-4b8e-b7b1-3318e82632de', { preview: '1440x492' })
-          "
-          media="(min-width: 1440px)"
-        />
-        <source
-          :srcset="
-            src('bb3bebbb-09d8-41d7-b89c-5e0cfa9bd855', { preview: '768x420' })
-          "
-          media="(min-width: 768px)"
-        />
-        <img
-          class="w-full absolute top-0 left-0 h-2/3 object-cover md:h-auto 2xl:h-full"
-          :src="
-            src('61839bda-3ff1-4b35-926b-833fcae20e61', { preview: '375x490' })
-          "
-        />
+        <source :srcset="src('2ba0480c-303b-4b8e-b7b1-3318e82632de', { preview: '1440x492' })
+          " media="(min-width: 1440px)" />
+        <source :srcset="src('bb3bebbb-09d8-41d7-b89c-5e0cfa9bd855', { preview: '768x420' })
+          " media="(min-width: 768px)" />
+        <img class="w-full absolute top-0 left-0 h-2/3 object-cover md:h-auto 2xl:h-full" :src="src('61839bda-3ff1-4b35-926b-833fcae20e61', { preview: '375x490' })
+          " />
       </picture>
-      <div
-        class="!px-0 py-5 absolute left-[--left] top-[--safe-area-padding-top] hidden 2xl:block z-20"
-        :style="{ '--left': bounding.x.value + 'px' }"
-      >
+      <div class="!px-0 py-5 absolute left-[--left] top-[--safe-area-padding-top] hidden 2xl:block z-20"
+        :style="{ '--left': bounding.x.value + 'px' }">
         <MoleculeBreadcrumb theme="light" />
       </div>
       <div
-        class="container py-10 md:h-[360px] md:flex md:flex-col md:justify-end md:items-start 2xl:h-auto 2xl:pt-10 2xl:pb-20 relative z-20"
-      >
-        <div
-          class="text-white space-y-4 md:max-w-5h md:space-y-3 2xl:max-w-7.25h"
-        >
+        class="container py-10 md:h-[360px] md:flex md:flex-col md:justify-end md:items-start 2xl:h-auto 2xl:pt-10 2xl:pb-20 relative z-20">
+        <div class="text-white space-y-4 md:max-w-5h md:space-y-3 2xl:max-w-7.25h">
           <h1 class="text-3xl md:text-4xl 2xl:text-5xl">Программа Трейд-ин</h1>
           <p class="text-base 2xl:text-lg">
             Предложение действует до 31/10/2024
           </p>
         </div>
-        <AtomButton
-          label="Заказать обратный звонок"
-          mode="full"
-          
-          class="mt-4 md:mt-5 2xl:mt-10"
-        />
+        <AtomButton label="Заказать обратный звонок" color="secondary" mode="full" class="mt-4 md:mt-5 2xl:mt-10" />
       </div>
     </div>
-    <UIContainer
-      class="pt-10 pb-12.5 2xl:pt-20 2xl:pb-10 bg-white text-primary"
-    >
+    <UIContainer class="pt-10 pb-12.5 2xl:pt-20 2xl:pb-10 bg-white text-primary">
       <div>
         <h1 class="font-semibold text-xl md:text-2xl 2xl:text-5xl">
           Выгодный обмен вашего автомобиля на новый Kia!
@@ -116,18 +90,14 @@ const { src } = useUploadcareSource()
               несколько этапов:
             </h3>
             <ul class="space-y-4 py-6 md:py-0 md:mt-4.5">
-              <li
-                v-for="i in [
-                  'Приехать в дилерский центр Kia',
-                  'Выбрать новый автомобиль',
-                  'Пройти оценку вашего автомобиля',
-                  'Заключить договор',
-                  'Сдать свой автомобиль в счет стоимости нового',
-                  'Уехать на новом автомобиле',
-                ]"
-                :key="i"
-                class="flex items-start gap-2.5"
-              >
+              <li v-for="i in [
+                'Приехать в дилерский центр Kia',
+                'Выбрать новый автомобиль',
+                'Пройти оценку вашего автомобиля',
+                'Заключить договор',
+                'Сдать свой автомобиль в счет стоимости нового',
+                'Уехать на новом автомобиле',
+              ]" :key="i" class="flex items-start gap-2.5">
                 <UICheckIcon class="size-5 shrink-0" />
                 <span>{{ i }}</span>
               </li>
@@ -136,16 +106,12 @@ const { src } = useUploadcareSource()
           <div>
             <h3 class="font-semibold">Преимущества Trade-in Kia:</h3>
             <ul class="space-y-4 py-6 md:py-0 md:mt-4.5">
-              <li
-                v-for="i in [
-                  'Выгодная оценка авто, идущего в зачет',
-                  'Весь процесс происходит не выходя из дилерского центра Kia, что экономит ваши силы и время',
-                  '100% прозрачная сделка и отсутствие юридических рисков',
-                  'Выгода 10 миллионов сум',
-                ]"
-                :key="i"
-                class="flex items-start gap-2.5"
-              >
+              <li v-for="i in [
+                'Выгодная оценка авто, идущего в зачет',
+                'Весь процесс происходит не выходя из дилерского центра Kia, что экономит ваши силы и время',
+                '100% прозрачная сделка и отсутствие юридических рисков',
+                'Выгода 10 миллионов сум',
+              ]" :key="i" class="flex items-start gap-2.5">
                 <UICheckIcon class="size-5 shrink-0" />
                 <span>{{ i }}</span>
               </li>
@@ -153,22 +119,14 @@ const { src } = useUploadcareSource()
           </div>
         </div>
         <p class="text-primary md:mt-7.5">
-          <b
-            >Kia Uzbekistan дорожит своей репутацией: работать с нами удобно и
-            надежно</b
-          >
+          <b>Kia Uzbekistan дорожит своей репутацией: работать с нами удобно и
+            надежно</b>
           Воспользуйтесь возможностью выгодно обменять свой автомобиль на
           новый.  Выбрав Kia, вы получаете высочайший уровень сервиса, комфорт и
           надежность, признанные во всем мире. Сделайте первый решительный шаг
           к большой цели вместе с Kia! Смелей!
         </p>
-        <AtomButton
-          label="Позвонить"
-          color="secondary"
-          mode="full"
-          
-          class="mt-6"
-        />
+        <AtomButton label="Позвонить" color="primary" mode="full" class="mt-6" />
       </div>
     </UIContainer>
   </div>
