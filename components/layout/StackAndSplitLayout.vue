f390f634f4982bc67ba987675fecc005c6a24e17
Rakhmatullo <qosimovrahmatullo006@gmail.com>
2025-01-22 17:19:14 +0500

    fix(components): split layout and stack and split layout class misplaced

--- components/layout/StackAndSplitLayout.vue
+++ components/layout/StackAndSplitLayout.vue
@@ -1,17 +1,18 @@
 <script setup lang="ts">
 const containerClasses = {
-  container: 'container 2xl:col-start-2 2xl:col-end-12 2xl:px-0 2xl:grid 2xl:grid-cols-10',
+  container: '2xl:grid 2xl:grid-cols-12 2xl:container 2xl:gap-x-grid-12-gap',
+  topContainer: 'container 2xl:col-start-2 2xl:col-end-12 2xl:px-0 2xl:grid 2xl:grid-cols-10',
   left: 'col-span-4',
   right: 'mt-2  md:mt-3 col-start-7 col-span-4 2xl:mt-0'
 }
 
-defineProps<{ title?: string, subtitle?: string, classNames?: Partial<{ left: string, right: string, topContainer: string, bottomContainer: string }> }>() 
+defineProps<{ title?: string, subtitle?: string, reverse?: boolean, classNames?: Partial<{ left: string, right: string, topContainer: string, bottomContainer: string, container: string }> }>() 
 </script>
 <template>
   <MoleculeSection ref="section" :section-title="title" :subtitle="subtitle"
-    class="2xl:grid 2xl:grid-cols-12 2xl:container 2xl:gap-x-grid-12-gap" align="left">
+    :class="[containerClasses.container, classNames?.container]" align="left">
     <template #title="{ sectionTitle, sectionTitleClass, subtitleClass, subtitle }">
-      <div :class="[containerClasses.container, classNames?.topContainer]">
+      <div :class="[containerClasses.topContainer, classNames?.topContainer]">
 
         <div :class="[containerClasses.left, classNames?.left]">
           <slot name="left" :sectionTitle :sectionTitleClass :subtitleClass :subtitle />
