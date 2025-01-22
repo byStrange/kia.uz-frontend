6e4f2e0afa91251e06053b26bf6384db2e715ed7
Rakhmatullo <qosimovrahmatullo006@gmail.com>
2025-01-22 10:58:35 +0500

    feat: layout and logic seperated from model ui organisms

---
+++ components/layout/SplitLayout.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+defineProps<{ title?: string, subtitle?: string, direction?: string }>() 
+</script>
+<template>
+  <MoleculeSection ref="section" :section-title="title" :subtitle="subtitle"
+    class="2xl:grid 2xl:grid-cols-12 2xl:container 2xl:gap-x-grid-12-gap " align="left" :class="{
+      '2xl:rtl bg-background': direction === 'left',
+    }">
+    <template #title="{ sectionTitle, sectionTitleClass, subtitleClass, subtitle }">
+      <div class="container split-left" :class="{
+
+        '2xl:px-0 col-span-4 col-start-2': true,
+        ltr: direction === 'left',
+      }">
+        <slot name="left" :sectionTitle :sectionTitleClass :subtitleClass :subtitle />
+      </div>
+      <div :class="{
+        'mt-10 col-start-7 col-span-6 2xl:mt-0 split-right': true,
+        ltr: direction === 'left',
+      }">
+        <slot name="right" />
+      </div>
+    </template>
+  </MoleculeSection>
+</template>
--- components/organisms/(models)/(model)/(index)/model-ui/ModelUITriple.vue
+++ components/organisms/(models)/(model)/(index)/model-ui/ModelUITriple.vue
@@ -58,45 +58,33 @@ onMounted(() => {
 })
 </script>
 <template>
-  <MoleculeSection ref="section" :section-title="section.title" :subtitle="section.subtitle"
-    class="2xl:grid 2xl:grid-cols-12 2xl:container 2xl:gap-x-grid-12-gap" align="left" :class="{
-      '2xl:rtl bg-background': section.direction === 'left',
-    }">
-    <template #title="{ sectionTitle, sectionTitleClass, subtitleClass, subtitle }">
-      <div class="container" :class="{
-        '2xl:col-start-2 2xl:col-end-12 2xl:px-0 2xl:grid 2xl:grid-cols-10':
-          true,
-        ltr: section.direction === 'left',
-      }">
+  <UIStackAndSplitLayout :title="section.title" :subtitle="section.subtitle" ref="section">
+    <template #left="{ sectionTitle, sectionTitleClass, subtitle, subtitleClass }">
+      <div>
         <p v-if="section.subtitle" :class="subtitleClass">{{ subtitle }}</p>
         <h1 :class="[
           sectionTitleClass,
-          {
-            'col-span-4': true,
-          },
+
         ]" class="!mb-0 organism-modelui-triple_title">
           {{ sectionTitle }}
         </h1>
-        <p :class="{
-          'mt-2 md:text-base+ md:mt-3 col-start-7 col-span-4 2xl:mt-0': true,
-        }" class="text-sm text-primary organism-modelui-triple_desc">
-          Откройте мир инноваций вместе с Kia Carnival — бесспорным лидером в
-          своем классе. Новый дизайн добавляет семейному кроссвэну изысканность,
-          роскошный интерьер обеспечивает комфорт, отличная управляемость
-          гарантирует уверенность на любой дороге. Новый Kia Carnival наполнит
-          вашу повседневную жизнь новыми впечатлениями.
-        </p>
       </div>
     </template>
-    <div :class="{
-      'mt-6 2xl:row-start-2 col-start-2 2xl:mt-1h col-end-12': true,
-      ltr: section.direction === 'left',
-    }">
+    <template #right>
+      <p class="text-sm md:text-base+ text-primary organism-modelui-triple_desc">
+        Откройте мир инноваций вместе с Kia Carnival — бесспорным лидером в
+        своем классе. Новый дизайн добавляет семейному кроссвэну изысканность,
+        роскошный интерьер обеспечивает комфорт, отличная управляемость
+        гарантирует уверенность на любой дороге. Новый Kia Carnival наполнит
+        вашу повседневную жизнь новыми впечатлениями.
+      </p>
+    </template>
+    <template #bottom>
       <picture v-if="section.images">
         <source v-if="section.images.lg" :srcset="section.images.lg?.src" media="(min-width: 1440px)" />
         <source v-if="section.images.md" :srcset="section.images.md?.src" media="(min-width: 768px)" />
         <img :src="section.images.default?.src" class="w-full" />
       </picture>
-    </div>
-  </MoleculeSection>
+    </template>
+  </UIStackAndSplitLayout>
 </template>
