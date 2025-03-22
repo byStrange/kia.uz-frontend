5fec54cb9af8f2b526ef9c7f871cba29226dcce3
Rakhmatullo <qosimovrahmatullo006@gmail.com>
2025-03-23 02:28:20 +0500

    feat: spare parts internationalization

--- pages/spare-parts.vue
+++ pages/spare-parts.vue
@@ -2,6 +2,7 @@
 import { useSvgAnnotator } from '~/composables/useSvgAnnotator';
 
 const { src } = useUploadcareSource()
+const { headerService } = useHeaderService()
 const { svgRef, addConnector, getSvgPoint, connectors, generatePath, removeConnector, opts } = useSvgAnnotator({
   breakPointOffset: 60,
   animationDuration: 800,
@@ -53,8 +54,6 @@ const handlePartClick = (x: number, y: number) => {
   }
 }
 
-const tab = ref<HTMLElement | null>(null)
-
 type Point = [number, number];
 
 
@@ -62,77 +61,124 @@ interface SparePart {
   name: string,
   imageCoordinates: Point
   image: string
+  title: string
+  description: string
 }
 
 const originalSparePartGroups = ref<{ name: string, parts: SparePart[] }[]>([
   {
-    name: 'Фильтр',
+    name: 'filter',
     parts: [
       {
-        name: 'Воздушный',
+        name: 'air',
         imageCoordinates: [896, 353],
-        image: src('30f3bbe2-7a6f-48e1-be15-121e1f61389e', { preview: '314x203' })
+        image: src('30f3bbe2-7a6f-48e1-be15-121e1f61389e', { preview: '314x203' }),
+        title: 'air_filter', description: 'air_filter_desc'
       },
       {
-        name: 'Масляный',
+        name: 'oil',
         imageCoordinates: [821, 462],
-        image: src('e1d897c3-1cb7-4a91-acde-5bcae9be2891', { preview: '288x173' })
+        image: src('e1d897c3-1cb7-4a91-acde-5bcae9be2891', { preview: '288x173' }),
+
+        title: 'oil_filter', description: 'oil_filter_desc'
       },
       {
-        name: 'Салонный',
+        name: 'cabin',
         imageCoordinates: [980, 297],
-        image: src('1c84c9da-8978-44a0-81f1-ccd0594a6385', { preview: '294x203' })
+        image: src('1c84c9da-8978-44a0-81f1-ccd0594a6385', { preview: '294x203' }),
+
+        title: 'cabin_filter', description: 'cabin_filter_desc'
       },
       {
-        name: 'Топливный',
+        name: 'fuel',
         imageCoordinates: [-860, -280],
-        image: src("cfe2dd83-8e4a-45a3-9052-a7377ca52b37", { preview: '294x191' })
+        image: src("cfe2dd83-8e4a-45a3-9052-a7377ca52b37", { preview: '294x191' }),
+        title: 'fuel_filter', description: 'fuel_filter_desc'
       }
     ]
   },
   {
-    name: 'Кузов',
+    name: 'car_body',
     parts: [
-      { name: 'Лобовое стекло', imageCoordinates: [12, 2], image: src('ad97ccec-0b25-4a3d-8bb5-92aae8d50aaa') },
-      { name: 'Кузовные запчасти', imageCoordinates: [12, 23], image: src('6b4f2e56-2ee9-4e55-bb06-8f511c6535ff') }
+      {
+        name: 'windshield',
+        imageCoordinates: [1080, 275],
+        image: src('ad97ccec-0b25-4a3d-8bb5-92aae8d50aaa'),
+        title: 'windshield', description: 'windshield_desc'
+      },
+      {
+        name: 'body_parts',
+        imageCoordinates: [1300, 510],
+        image: src('6b4f2e56-2ee9-4e55-bb06-8f511c6535ff'),
+        title: 'body_parts', description: 'body_parts_desc'
+      }
     ]
   },
 
   {
-    name: 'Двигатель',
+    name: 'engine',
     parts: [
       {
-        name: 'Масло',
-        imageCoordinates: [0, 0],
+        name: 'engine_oil',
+        imageCoordinates: [790, 400],
         image: src('d4524d97-71ab-4651-8207-e5c1426391dc', {
           crop: '190x200/45,0',
-        })
+        }),
+
+        title: 'engine_oil', description: 'engine_oil_desc'
       },
       {
-        name: 'Свечи зажигания',
-        imageCoordinates: [12, 21],
-        image: src('bba08360-3e81-401f-b76f-0a9a75c6b1e9')
+        name: 'spark_plugs',
+        imageCoordinates: [942, 425],
+        image: src('bba08360-3e81-401f-b76f-0a9a75c6b1e9'),
+
+        title: 'spark_plugs', description: 'spark_plugs_desc'
       },
     ]
   },
 
   {
-    name: 'Тормоза',
+    name: 'brakes',
     parts: [
-      { name: 'Тормозные диски', imageCoordinates: [12, 2], image: src('13417e07-ab22-44f6-bf23-d6507de26b48') },
-      { name: 'Тормозные колодки', imageCoordinates: [12, 23], image: src('9ec56b41-fcc3-4323-9fd9-cbbffa101b37') }
+      {
+
+        name: 'brake_discs',
+        imageCoordinates: [1070, 558],
+        image: src('13417e07-ab22-44f6-bf23-d6507de26b48'),
+        title: 'brake_discs', description: 'brake_discs_desc'
+      },
+      {
+        name: 'brake_pads',
+        imageCoordinates: [1110, 495],
+        image: src('9ec56b41-fcc3-4323-9fd9-cbbffa101b37'),
+        title: 'brake_pads', description: 'brake_pads_desc'
+      }
+
     ]
   }
   , {
-    name: 'Комплектующие',
+    name: 'components',
     parts: [
       {
-        name: 'Антифриз', imageCoordinates: [12, 23], image: src('100cc81d-c299-424f-90b8-9db7677d864f', {
+        name: 'antifreeze',
+        imageCoordinates: [896, 353],
+        image: src('100cc81d-c299-424f-90b8-9db7677d864f', {
           crop: '190x200/45,0',
-        })
+        }),
+        title: 'antifreeze', description: 'antifreeze_desc'
       },
-      { name: 'Щётки стеклоочистителя', imageCoordinates: [12, 32], image: src('d0274b1c-8ffb-409d-8b07-f5a358f7c79a') },
-      { name: 'Амортизаторы', imageCoordinates: [2, 332], image: src('c6a2c2f4-1ada-4669-92c4-ca73bb2c680c') }
+      {
+        name: 'wiper_blades',
+        imageCoordinates: [1125, 340],
+        image: src('d0274b1c-8ffb-409d-8b07-f5a358f7c79a'),
+        title: 'wiper_blades', description: 'wiper_blades_desc'
+      },
+      {
+        name: 'shock_absorbers',
+        imageCoordinates: [1125, 340],
+        image: src('c6a2c2f4-1ada-4669-92c4-ca73bb2c680c'),
+        title: 'shock_absorbers', description: 'shock_absorbers_desc'
+      }
 
     ]
   }
@@ -183,24 +229,18 @@ definePageMeta({
         </UIDesktopOnly>
         <div class="py-20 space-y-7.5 2xl:py-0 2xl:mt-6 2xl:max-w-7h">
           <div class="space-y-4">
-            <h1 class="text-2xl md:text-4xl 2xl:py-3 2xl:text-5xl">Запчасти</h1>
-            <p class="text-base md:text-lg 2xl:max-w-6.5h">Производитель обеспечивает своих дилеров необходимым
-              количеством
-              запчастей,
-              деталей
-              и комплектующих для
-              профессионального ремонта. Оригинальные запчасти обеспечивают высокое качество ремонтных и сервисных
-              работ.
+            <h1 class="text-2xl md:text-4xl 2xl:py-3 2xl:text-5xl">{{ $t('common.spare_parts') }}</h1>
+            <p class="text-base md:text-lg 2xl:max-w-6.5h">{{ $t('spare_parts.manufacturer_supplies_original_parts') }}
             </p>
           </div>
           <div class='space-y-4 2xl:space-y-0 2xl:flex 2xl:gap-x-7.5 2xl:pt-3'>
             <div class="flex gap-x-2">
               <UIShieldIcon />
-              <p class="text-base">Безопасность превыше всего</p>
+              <p class="text-base">{{ $t('spare_parts.safety_first') }}</p>
             </div>
             <div class="flex gap-x-2">
               <UICheckstrokeIcon />
-              <p class="text-base">Надежность, проверенная исследованиями</p>
+              <p class="text-base">{{ $t('spare_parts.reliability_proven_by_research') }}</p>
             </div>
           </div>
         </div>
@@ -208,28 +248,27 @@ definePageMeta({
     </div>
 
     <div class="pb-15 relative">
-      <MoleculeTabsContainer :tabs="['Оригинальные запчасти', 'Запчасти PL2',]" :is-header-center="false"
-        header-container-class="w-fit mx-0"
-        :is-content-full="true"
+      <MoleculeTabsContainer :tabs="[$t('spare_parts.original_parts'), $t('spare_parts.parts_pl2')]"
+        :is-header-center="false" header-container-class="w-fit mx-0" :is-content-full="true"
         header-class="overflow-x-auto no-scrollbar text-nowrap !mt-7.5 2xl:!px-0 2xl:ml-[--padding-left]"
         class="pt-5 relative z-10" :is-header-full="true">
 
         <template #1>
           <div>
             <UIContainer class="md:grid md:grid-cols-3 md:gap-y-6 2xl:flex pb-2.5 relative z-10">
-              <div class="space-y-1.5 py-2.5 md:pt-0 2xl:pr-7.5" v-for="group in originalSparePartGroups">
+              <div v-for="group in originalSparePartGroups" class="space-y-1.5 py-2.5 md:pt-0 2xl:pr-7.5">
                 <div class="text-caption pl-6 py-1">
-                  <span class="text-base">{{ group.name }}</span>
+                  <span class="text-base">{{ $t(`spare_parts.${group.name}`) }}</span>
                 </div>
                 <div>
-                  <div
+                  <div v-for="part in group.parts"
                     class="pl-6 py-1 2xl:pr-2.5 hover:bg-background transition-colors duration-300 cursor-pointer relative"
-                    v-for="part in group.parts" @click="selectedPart = part">
+                    @click="selectedPart = part">
                     <Transition name="slide-fade">
 
                       <UIArrowRightIcon v-if="selectedPart == part" class="absolute left-0" />
                     </Transition>
-                    <span class="text-base select-none">{{ part.name }}</span>
+                    <span class="text-base select-none">{{ $t(`spare_parts.${part.name}`) }}</span>
                   </div>
                 </div>
               </div>
@@ -237,42 +276,36 @@ definePageMeta({
 
             <UIContainer>
               <div class="2xl:max-w-[480px] relative z-10">
-                <div class="relative md:h-2h flex justify-center items-center min-h-1.5h" ref="partImage">
+                <div ref="partImage" class="relative md:h-2h flex justify-center items-center min-h-1.5h">
                   <Transition name="slide-fade" mode="out-in">
                     <img :key="selectedPart.name" :src="selectedPart.image" alt="Part image" class="md:absolute">
                   </Transition>
                 </div>
 
-                <div class="space-y-7.5 pt-6 pb-[70px] 2xl:p-0 2xl:mt-6">
-                  <div class="space-y-6">
-                    <h1 class="text-lg font-semibold md:text-2xl">Воздушный фильтр</h1>
+                <div class="space-y-7.5 pt-6 pb-[70px] 2xl:p-0 2xl:mt-6 2xl:min-h-3h">
+                  <div v-if="selectedPart" class="space-y-6">
+                    <h1 class="text-lg font-semibold md:text-2xl">{{ $t(`spare_parts.${selectedPart.title}`) }}</h1>
+
                     <div class="space-y-2.5">
-                      <div class="flex gap-x-2 items-start">
+                      <div v-for="item in $t(`spare_parts.${selectedPart.description}`).split('&')" :key="item"
+                        class="flex gap-x-2 items-start">
                         <UICheckOnIcon class="text-primary shrink-0" />
                         <p class="text-base">
-                          Для работы двигателю необходима определенная пропорция смеси топлива и воздуха. Воздух
-                          поступает
-                          в систему
-                          смесеобразования через воздушный фильтр.
+                          {{ item }}
                         </p>
-                      </div>
-                      <div class="flex gap-x-2 items-start">
-                        <UICheckOnIcon class="text-primary shrink-0" />
-                        <p class="text-base">Воздушный фильтр обеспечивает защиту двигателя от загрязнений и инородных
-                          частиц,
-                          вызывающих быстрый износ деталей поршневой группы. Фильтр разработан в соответствии со
-                          спецификациями и
-                          пропускает тот объем воздуха, который предусмотрен для двигателя.</p>
                       </div>
                     </div>
                   </div>
-                  <AtomButton color="primary" label="Позвонить" mode="full" />
+
+                  <AtomLink :to="`tel:${headerService.phoneLine1}`" :label="$t('common.call')" mode="full"
+                    color="primary" />
+
                 </div>
               </div>
             </UIContainer>
-            <div class="fixed left-20 top-20 w-40 z-40" v-if="false">
-              <input type="number" v-model="testPos.x">
-              <input type="number" v-model="testPos.y">
+            <div v-if="false" class="fixed left-20 top-20 w-40 z-40">
+              <input v-model="testPos.x" type="number">
+              <input v-model="testPos.y" type="number">
               <span>{{ testPos.x + ', ' + testPos.y }}</span>
             </div>
             <UIDesktopOnly>
@@ -318,10 +351,9 @@ definePageMeta({
           <div>
             <UIContainer class="pt-7 pb-7.5 2xl:pb-12.5">
               <div class="space-y-6 2xl:max-w-6h">
-                <h1 class="text-lg text-primary font-semibold md:text-2xl 2xl:text-3xl">Запасные части PL2</h1>
-                <p>Решили пересмотреть бюджет на обслуживание вашего Kia? Понимаете и цените преимущества официального
-                  сервиса? Выбирайте запасные части PL2 — неизменное качество за разумные деньги.
-                </p>
+                <h1 class="text-lg text-primary font-semibold md:text-2xl 2xl:text-3xl">{{
+                  $t('spare_parts.spare_parts_pl2') }}</h1>
+                <p>{{ $t('spare_parts.service_budget_review_pl2') }}</p>
               </div>
             </UIContainer>
             <div class="py-10 bg-background">
@@ -334,32 +366,24 @@ definePageMeta({
               }">
               <div class="space-y-10 2xl:space-y-12.5">
                 <div>
-                  <p>Спрашивайте запасные части PL2 у официальных дилеров и получайте:</p>
+                  <p>{{ $t('spare_parts.pl2_parts_request_dealers') }}</p>
 
                   <div class="space-y-2.5 pl-4 mt-4">
-                    <div class="flex gap-x-2" v-for="i in 4">
+                    <div v-for="item in $t('spare_parts.pl2_parts_requests').split('&')" :key="item"
+                      class="flex gap-x-2">
                       <UICheckOnIcon class="shrink-0" />
-                      <p class="text-base">Качественные запчасти разработанные и произведенные для автомобилей Kia
-                        под контролем
-                        автопроизводителя;</p>
+                      <p class="text-base">{{ item }}</p>
                     </div>
                   </div>
                 </div>
-                <p>
-                  Если вы владелец одной из этих моделей Rio QB, cee’d ED, cee’d JD, Optima TF, Picanto TA, Soul AM,
-                  Sportage SL, Sorento XM у вас есть еще один повод оставаться лояльным Kia — оставаться с официальным
-                  сервисом.
-                </p>
+                <p>{{ $t('spare_parts.official_service_loyalty') }}</p>
               </div>
-              <AtomButton label="Позвонить" mode="full" color="primary" />
+              <AtomLink :to="`tel:${headerService.phoneLine1}`" :label="$t('common.call')" mode="full"
+                color="primary" />
             </UIContainer>
           </div>
-
         </template>
       </MoleculeTabsContainer>
-
-
     </div>
-
   </UISafeAreaView>
 </template>
