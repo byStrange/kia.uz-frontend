958f97016bb7a3b19049770080db22a2597dabce
Rakhmatullo <qosimovrahmatullo006@gmail.com>
2025-04-23 14:21:53 +0500

    style(ConfigurationAndFeaturesList): clean up formatting and spacing in template code

--- components/organisms/ConfigurationAndFeaturesList.vue
+++ components/organisms/ConfigurationAndFeaturesList.vue
@@ -36,15 +36,13 @@ defineProps<{ standardFeatures: ModelPricingAndDetailsPage['standard_features'],
 <template>
   <div>
     <div class="bg-background flex w-full relative 2xl:bg-white">
-      <ElementSlideView
-ref="confSwiper" class="w-full" :paginator="false" :data="configurations || []"
+      <ElementSlideView ref="confSwiper" class="w-full" :paginator="false" :data="configurations || []"
         navigation-mode="oneside-left" navigation-type="sm" :breakpoints-enabled="false" :breakpoints="{
           768: { slidesOffsetBefore: 37, slidesOffsetAfter: 37 },
           1440: { slidesOFfsetBefore: 20, slidesOffsetAfter: 20 },
         }" :slides-offset-before="15" :slides-offset-after="15" swiper-slide-class="!w-fit">
         <template #slide="{ item: configuration }">
-          <div
-class="p-4 md:p-5 w-[172px] md:w-[232px] 2xl:w-[220px]" :class="{
+          <div class="p-4 md:p-5 w-[172px] md:w-[232px] 2xl:w-[220px]" :class="{
             'hoverable:border hoverable:border-transparent hoverable:hover:border-primary hoverable:transition-colors cursor-pointer': !showConfigurationSelectButton
           }">
             <div>
@@ -59,8 +57,7 @@ class="p-4 md:p-5 w-[172px] md:w-[232px] 2xl:w-[220px]" :class="{
             <h2 class="space-x-1 md:mt-4 mt-3 text-base font-semibold text-primary">
               <span>{{ formatPrice(configuration.price, $t('prefixes.sum')) }}</span>
             </h2>
-            <AtomButton
-v-if="showConfigurationSelectButton" label="Выбрать" color="secondary" size="xs"
+            <AtomButton v-if="showConfigurationSelectButton" label="Выбрать" color="secondary" size="xs"
               class="w-full hidden md:flex md:mt-4 2xl:mt-5" @click="$emit('chooseConfiguration', configuration.id)" />
           </div>
         </template>
@@ -71,16 +68,14 @@ v-if="showConfigurationSelectButton" label="Выбрать" color="secondary" si
       </ElementSlideView>
     </div>
 
-    <UIContainer
-class="pt-5 pb-12 md:pt-10 md:pb-15 2xl:px-0 2xl:max-w-none 2xl:pl-grid-12-gap 2xl:pr-[--padding-x]"
+    <UIContainer class="pt-5 pb-12 md:pt-10 md:pb-15 2xl:px-0 2xl:max-w-none 2xl:pl-grid-12-gap 2xl:pr-[--padding-x]"
       :style="{
         '--padding-x': bounding.x.value + 'px',
       }">
-      <MoleculeAccordion
-:default-open="true" :classes="{
+      <MoleculeAccordion :default-open="true" :classes="{
         contentContainer: 'duration-700',
         contentWrapper: 'duration-700',
-      }" :items="[{ label:$t('common.standard_features'), content: '' }]">
+      }" :items="[{ label: $t('common.standard_features'), content: '' }]">
 
         <template #expandicon>
           <UITickToBottom class="min-w-5" />
@@ -90,7 +85,7 @@ class="pt-5 pb-12 md:pt-10 md:pb-15 2xl:px-0 2xl:max-w-none 2xl:pl-grid-12-gap 2
           <div
             class="py-3 cursor-pointer flex justify-between w-full items-center text-base font-semibold text-primary text-left md:py-4 md:text-lg 2xl:text-2xl border-b-2 border-protection"
             @click="toggle">
-            <span>{{$t('common.standard_features')}}</span>
+            <span>{{ $t('common.standard_features') }}</span>
             <button @click="toggle">
               <UITickToBottom class="text-primary transition-transform" :class="{ '!rotate-180': expanded }" />
             </button>
@@ -113,8 +108,7 @@ class="pt-5 pb-12 md:pt-10 md:pb-15 2xl:px-0 2xl:max-w-none 2xl:pl-grid-12-gap 2
         </template>
       </MoleculeAccordion>
 
-      <MoleculeAccordion
-:classes="{
+      <MoleculeAccordion :classes="{
         contentContainer: 'duration-700',
         contentWrapper: 'duration-700',
       }"
@@ -132,8 +126,7 @@ class="pt-5 pb-12 md:pt-10 md:pb-15 2xl:px-0 2xl:max-w-none 2xl:pl-grid-12-gap 2
               {{ item.label }}
             </span>
             <button @click="toggle">
-              <UITickToBottom
-class="text-primary transition-transform duration-700"
+              <UITickToBottom class="text-primary transition-transform duration-700"
                 :class="{ '!rotate-180': expanded }" />
             </button>
           </div>
@@ -144,13 +137,11 @@ class="text-primary transition-transform duration-700"
             <div v-for="feature in content" :key="feature.id" class="py-4">
               <p class="text-sm">{{ feature.name }}</p>
               <div class="mt-3.5 overflow-hidden">
-                <div
-class="flex translate-x-[--translate-x] transition-transform" :style="{
+                <div class="flex translate-x-[--translate-x] transition-transform" :style="{
                   '--translate-x':
                     (confSwiperClientX ? confSwiperClientX - 15 : 0) + 'px',
                 }">
-                  <div
-v-for="config in configurations || []" :key="config.name"
+                  <div v-for="config in configurations || []" :key="config.name"
                     class="w-[172px] md:w-[232px] shrink-0 2xl:w-[220px]">
                     <div v-if="hasFeature(feature.id, config)" class="size-2.5 bg-primary rounded-full" />
                     <div v-else>-</div>
