d1ac2928a41bf59585ec350e4c775f5ccafcfedc
Rakhmatullo <qosimovrahmatullo006@gmail.com>
2025-05-10 08:52:39 +0500

    feat: lock header after page is built

--- middleware/route.global.ts
+++ middleware/route.global.ts
@@ -1,14 +1,17 @@
 export default defineNuxtRouteMiddleware((to, from) => {
   if (import.meta.client) {
     const { lockHeader, unlockHeader, closeExtendedMenu } = useHeaderService()
+    const nuxtApp = useNuxtApp()
 
-    // some pages might require locking the header (read more about locking in the ~composables/useHeaderService)
-    // pages that require locking header will have meta.lockHover set to true
-    if (to.meta?.lockHover) lockHeader()
-    else unlockHeader()
+    nuxtApp.hook('page:finish', () => {
+      // some pages might require locking the header (read more about locking in the ~composables/useHeaderService)
+      // pages that require locking header will have meta.lockHover set to true
+      if (to.meta?.lockHover) lockHeader()
+      else unlockHeader()
 
-    // scroll to top
-    if (to.path !== from.path) window.scrollTo(0, 0)
+      // scroll to top
+      if (to.path !== from.path) window.scrollTo(0, 0)
+    })
 
     // close extended menu on route change
     closeExtendedMenu()
